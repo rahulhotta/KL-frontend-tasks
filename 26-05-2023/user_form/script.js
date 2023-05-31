8160c8128fb6b508995520d5a2dbbbe0d3569d27
Rahul kumar hotta <rahulkumar.hotta@corp.knowledgelens.com>
2023-05-31 09:49:05 +0530

    made code imporvements

--- 26-05-2023/user_form/script.js
+++ 26-05-2023/user_form/script.js
@@ -1,19 +1,21 @@
-function ValidateEmail(input) {
+// Function to validate Email using regex
+function validateEmail(input) {
   let validRegex = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
   let inputErrorFeild = document.getElementById("email_error_msg");
   let emailInputField = document.getElementById("email_input");
-  console.log(emailInputField)
+  console.log(emailInputField);
   if (validRegex.test(input) && input.trim() !== "") {
     inputErrorFeild.innerHTML = "";
     emailInputField.style.borderColor = "black";
     return true;
   } else {
-    // document.userForm.email_input.focus();
     inputErrorFeild.innerHTML = "Please enter a valid Email";
     emailInputField.style.borderColor = "red";
     return false;
   }
 }
+
+// Function to validate the fields which are mandatory to input
 const validateMandatory = (text, name) => {
   let nameErrorField = document.getElementById(`${name}_error_msg`);
   let inputField = document.getElementById(`${name}`);
@@ -22,11 +24,13 @@ const validateMandatory = (text, name) => {
     inputField.style.borderColor = "black";
     return true;
   } else {
-    nameErrorField.innerHTML = "This feild can not be empty";
+    nameErrorField.innerHTML = "This field can not be empty";
     inputField.style.borderColor = "red";
     return false;
   }
 };
+
+// Function to validate the phone number using regex
 let validatePhone = (input) => {
   let validRegex = new RegExp("^[7-9][0-9]{9}$");
   let inputErrorFeild = document.getElementById("phone_input_error_msg");
@@ -44,21 +48,40 @@ let validatePhone = (input) => {
     return true;
   }
 };
+let emailValue = document.userForm.email_input;
+let userName = document.userForm.name_input;
+let phoneNumber = document.userForm.phone_input;
+let city = document.userForm.city_input;
+let state = document.userForm.state_input;
 let userForm = document.getElementById("userForm");
+
+// list of feilds which are mandatory to input
+const userDataList = [userName, city, state];
+
+userDataList.forEach((item) => {
+  item.addEventListener("change", () => {
+    formFlag = validateMandatory(item.value, item.name) && formFlag;
+  });
+});
+
+emailValue.addEventListener("change", () => {
+  validateEmail(emailValue.value);
+});
+phoneNumber.addEventListener("change", () => {
+  validatePhone(phoneNumber.value);
+});
+
+// an event listener to handle form validation on submit
 userForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  let emailValue = document.userForm.email_input;
-  let userName = document.userForm.name_input;
-  let phoneNumber = document.userForm.phone_input;
-  let city = document.userForm.city_input;
-  let state = document.userForm.state_input;
-  if (
-    validateMandatory(userName.value, userName.name) &&
-    ValidateEmail(emailValue.value) &&
-    validateMandatory(city.value, city.name) &&
-    validateMandatory(state.value, state.name) &&
-    validatePhone(phoneNumber.value)
-  ) {
+  let formFlag = validatePhone(phoneNumber.value);
+  formFlag = validateEmail(emailValue.value) && formFlag;
+  userDataList.forEach((item) => {
+    formFlag = validateMandatory(item.value, item.name) && formFlag;
+  });
+
+  // making all the input values empty after successful submission of form
+  if (formFlag) {
     emailValue.value = "";
     userName.value = "";
     phoneNumber.value = "";
